c0d29a84a15399dd9765906c2b3876b41a25ea6e
James Bevins <james.e.bevins@gmail.com>
2017-05-08 08:42:14 -0700

    Added _init_.py

--- _init_.py
+++ _init_.py
@@ -1,1 +1,16 @@
+"""!
+@package Gnowee
 
+Contains the Gnowee optimization program and associated utilities.
+
+\sa Gnowee
+\sa Gnoweeheuristics
+\sa GnoweeUtilities
+\sa ObjectiveFunctions
+\sa OptiPlot
+\sa Sampling
+\sa Utilities
+"""
+
+__all__ = ["Gnowee", "Gnoweeheuristics", "GnoweeUtilities",
+           "ObjectiveFunctions", "OptiPlot", "Sampling", "Utilities"]
