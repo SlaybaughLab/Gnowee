314e72c3ea7a198dfc7d0da9cf5af113bdd72a30
James Bevins <james.e.bevins@gmail.com>
2017-05-08 17:25:07 -0700

    Updated Event and Parent classes in GnoweeUtilities

--- src/GnoweeUtilities.py
+++ src/GnoweeUtilities.py
@@ -1,142 +1,198 @@
-#######################################################################################################
-#
-# Module : GnoweeUtilities.py
-#
-# Contains : Functions and methods useful across the suite of Gnowee algorithms.
-#
-# Author : James Bevins
-#
-# Last Modified: 18Apr17
-#
-#######################################################################################################
+"""!
+@file src/GnoweeUtilities.py
+@package Gnowee
+
+@defgroup GnoweeUtilities GnoweeUtilities
+
+@brief Classes and methods to support the Gnowee optimization algorithm.
+
+@author James Bevins
+
+@date 8May17
+"""
 
 import numpy as np
 import copy as cp
 import bisect
 
-import GnoweeHeuristics as gh#import Initialize
+from GnoweeHeuristics import initialize
 
-#---------------------------------------------------------------------------------------# 
-class Parent:
+#------------------------------------------------------------------------------#
+class Parent(object):
+    """!
+    @ingroup GnoweeUtilities
+    The class contains all of the parameters pertinent to a member of the
+    population.
     """
-    Creates an object representing a current design and associated parameters
+
+    ##
+    def __init__(self, variables=None, fitness=1E15, changeCount=0,
+                 stallCount=0):
+        """!
+        Constructor to build the Parent class.
+
+        @param self: <em> Parent pointer </em> \n
+            The Parent pointer. \n
+        @param variables: \e array \n
+            The set of variables representing a design solution. \n
+        @param fitness: \e float \n
+            The assessed fitness for the current set of variables. \n
+        @param changeCount: \e integer \n
+            The number of improvements to the current population member. \n
+        @param stallCount: \e integer \n
+            The number of evaluations since the last improvement. \n
+        """
+
+        ##  @var variables
+        # \e array
+        # The set of variables representing a design solution.
+        self.variables = variables
+
+        ##  @var fitness
+        # \e float
+        # The assessed fitness for the current set of variables.
+        self.fitness = fitness
+
+        ##  @var changeCount
+        # \e integer
+        # The number of improvements to the current population member.
+        self.changeCount = changeCount
+
+        ##  @var stallCount
+        # \e integer
+        # he number of evaluations since the last improvement.
+        self.stallCount = stallCount
+
+    def __repr__(self):
+        """!
+        Parent print function.
+
+        @param self: <em> Parent pointer </em> \n
+            The Parent pointer. \n
+        """
+        return "Parent({}, {}, {}, {})".format(self.variables, self.fitness,
+                                               self.changeCount,
+                                               self.stallCount)
+
+    def __str__(self):
+        """!
+        Human readable Parent print function.
+
+        @param self: <em> Parent pointer </em> \n
+            The Parent pointer. \n
+        """
+
+        header = ["Parent:"]
+        header += ["Variables = {}".format(self.variables)]
+        header += ["Fitness = {}".format(self.fitness)]
+        header += ["Change Count = {}".format(self.changeCount)]
+        header += ["Stall Count = {}".format(self.stallCount)]
+        return "\n".join(header)+"\n"
+
+#------------------------------------------------------------------------------#
+class Event(object):
+    """!
+    @ingroup GnoweeUtilities
+    Represents a snapshot in the optimization process to be used for debugging,
+    benchmarking, and user feedback.
+    """
+
+    ##
+    def __init__(self, generation, evaluations, fitness, design):
+        """!
+        Constructor to build the Event class.
+
+        @param self: <em> Event pointer </em> \n
+            The Event pointer. \n
+        @param generation: \e integer \n
+            The generation the design was arrived at. \n
+        @param evaluations: \e integer \n
+            The number of fitness evaluations done to obtain this design. \n
+        @param fitness: \e float \n
+            The assessed fitness for the current set of variables. \n
+        @param design: \e array \n
+            The set of variables representing a design solution. \n
+        """
+
+        ##  @var generation
+        # \e integer
+        # The generation the design was arrived at.
+        self.generation = generation
+
+        ##  @var evaluations
+        # \e integer
+        # The number of fitness evaluations done to obtain this design.
+        self.evaluations = evaluations
+
+        ##  @var fitness
+        # \e float
+        # The assessed fitness for the current set of variables.
+        self.fitness = fitness
+
+        ##  @var design
+        # \e array
+        # The set of variables representing a design solution.
+        self.design = design
+
+    def __repr__(self):
+        """!
+        Event print function.
+
+        @param self: <em> Event pointer </em> \n
+            The Event pointer. \n
+        """
+        return "Event({}, {}, {}, {})".format(self.generation, self.evaluations,
+                                               self.fitness, self.design)
+
+    def __str__(self):
+        """!
+        Human readable Event print function.
+
+        @param self: <em> Event pointer </em> \n
+            The Event pointer. \n
+        """
+
+        header = ["Event:"]
+        header += ["Generation # = {}".format(self.generation)]
+        header += ["Evaluation # = {}".format(self.evaluations)]
+        header += ["Fitness = {}".format(self.fitness)]
+        header += ["Design = {}".format(self.design)]
+        return "\n".join(header)+"\n"
+
+#---------------------------------------------------------------------------------------#
+class WeightedRandomGenerator(object):
+    """
+    Defines a class of weights to be used to select number of instances in array randomly with
+    linear weighting.  
    
-    Attributes
+    Parameters
     ==========
-    fitness : float
-        The assessed design fitness
-    design_variables : array
-        The variables representing the design solution 
-        [Used for continuous or discrete optimization]
-    changes : integer
-         The number of improvements found to the current population member
-    stall : integer
-         The number of evals since last improvement
+    self : object
+        Current instance of the class
+    weights : array
+        The array of weights (Higher = more likely to be selected)
+   
     Returns
     =======
-    None
+    bisect.bisect_right(self.totals, rnd) : integer
+        The randomly selected index of the weights array
     """
-        
-    def __init__(self,fitness=1E15,design_variables=[],changes=0,stall=0):
-        self.f=fitness
-        self.d=design_variables
-        self.c=changes
-        self.s=stall
-        
-        
-#---------------------------------------------------------------------------------------#  
-class Event:
-    """
-    Creates an event object representing a snapshot in the optimization process
-   
-    Attributes
-    ==========
-    generation : integer
-        The generation the design was arrived at
-    evaluations : integer
-        The number of fitness evaluations done to obtain this design
-    fitness : scalar
-        The assessed design fitness
-    design : array
-        The variables representing the design solution
-    Returns
-    =======
-    None
-    """
-        
-    def __init__(self,generation,evaluations,fitness,design):
-        self.g=generation
-        self.e=evaluations
-        self.f=fitness
-        self.d=design   
-        
-#---------------------------------------------------------------------------------------#    
-class Settings:
-    """
-    Creates a object representing the settings for the optimization algorithm
-   
-    Attributes
-    ==========
-    population : int
-        The number of members in each generation (Default: 25)
-    initial_sampling : string
-        The method used to sample the phase space and create the initial population (Default: 'random')
-        Valid('random','nolh','nolh-rp','nolh-cdr',and 'lhc') 
-    frac_discovered : scalar
-        Discovery probability (Default: 0.25)
-    frac_elite : scalar
-        Elite fraction (Default: 0.2)
-    max_gens : int
-        The maximum number of generations to search (Default: 10000)
-    feval_max : int
-        The maximum number of objective function evaluations (Default: 100000)
-    conv_tol : scalar
-        The minimum change of the best objective value before the search
-        terminates (Default: 1e-5)
-    stall_iter_limit : int
-        The maximum number of genrations to search without a descrease
-        exceeding conv_tol (Default: 200)       
-    optimal_fitness : scalar
-        The best know fitness value for the problem considered (Default: 0)
-    opt_conv_tol : scalar
-        The maximum deviation from the best know fitness value before the search
-        terminates (Default: 1e-2) 
-    alpha : scalar
-        Levy exponent - defines the index of the distribution and controls scale properties of the stochastic process
-        (Default: 1.5)
-    gamma : scalar
-        Gamma - Scale unit of process for Levy flights (Default: 1)
-    n : integer
-        Number of independent variables - can be used to reduce Levy flight variance (Default: 1)
-    scaling_factor : scalar
-        Step size scaling factor used to adjust Levy flights to length scale of system (Default: 10)
-    penalty : scalar
-        Individual constraint violation penalty to objective function (Default: 0.0)
-    Returns
-    =======
-    None
-    """
-        
-    def __init__(self,population=25,initial_sampling='random',frac_discovered=0.25,frac_elite=0.2, frac_levy=0.4,
-                 max_gens=20000, feval_max=200000,conv_tol=1e-6,stall_iter_limit=400,optimal_fitness=0,
-                 opt_conv_tol=1e-2,alpha=1.5, gamma=1,n=1,scaling_factor=10.0, penalty=0.0):          
-        self.p=population
-        self.s=initial_sampling           
-        self.fd=frac_discovered 
-        self.fe=frac_elite         
-        self.fl=frac_levy
-        self.gm=max_gens            
-        self.em=feval_max       
-        self.ct=conv_tol              
-        self.sl=stall_iter_limit         
-        self.of=optimal_fitness        
-        self.ot=opt_conv_tol             
-        self.a=alpha                    
-        self.g=gamma                  
-        self.n=n                        
-        self.sf=scaling_factor  
-        self.pen=penalty
-        
+    
+    def __init__(self, weights):
+        self.totals = []
+        running_total = 0
+
+        for w in weights:
+            running_total += w
+            self.totals.append(running_total)
+
+    def next(self):
+        rnd = np.random.rand() * self.totals[-1]
+        return bisect.bisect_right(self.totals, rnd)
+
+    def __call__(self):
+        return self.next()
+
 #---------------------------------------------------------------------------------------#
 def Get_Best(func,parents,children,lb,ub,varType,timeline,S,genUpdate,indices=[],mhFrac=0.0,random_replace=False,discreteID=[],discreteMap=[[]]):
     """
@@ -224,14 +280,14 @@ def Get_Best(func,parents,children,lb,ub,varType,timeline,S,genUpdate,indices=[]
             replace+=1
             if parents[i].c>=25 and j>=S.p*S.fe:
                 #print "Max Changes: ", parents[i].f
-                parents[i].d=gh.Initialize(1, 'random', lb, ub, varType).flatten()
+                parents[i].d=initialize(1, 'random', lb, ub, varType).flatten()
                 (fnew,gnew)=func(parents[i].d,penalty=S.pen)
                 parents[i].f=fnew
                 parents[i].c=0
         else:
             parents[j].s+=1
             if parents[j].s>50000 and j!=0:
-                parents[i].d=gh.Initialize(1, 'random', lb, ub, varType).flatten()
+                parents[i].d=initialize(1, 'random', lb, ub, varType).flatten()
                 (fnew,gnew)=func(parents[i].d,penalty=S.pen)
                 parents[i].f=fnew
                 parents[i].c=0
@@ -274,113 +330,3 @@ def Get_Best(func,parents,children,lb,ub,varType,timeline,S,genUpdate,indices=[]
         timeline[-1].e+=feval
         
     return(parents,replace,timeline)
-
-#---------------------------------------------------------------------------------------#
-def Simple_Bounds(tmp,lb,ub):
-    """
-    Application of problem boundaries to generated solutions
-   
-    Parameters
-    ==========
-    tmp : array
-        The proposed new system designs
-    lb : array
-        The lower bounds of the design variable(s)
-    ub : array
-        The upper bounds of the design variable(s)
-   
-    Returns
-    =======
-    tmp : array
-        The new system designs that are within problem boundaries
-    """
-    
-    assert len(tmp)==len(lb), 'tmp and lb best have different # of design variables in Simple_Bounds function.'
-    assert len(ub)==len(lb), 'Boundaries best have different # of design variables in Simple_Bounds function.'
-            
-    #Apply lower bound
-    for i in range(0,len(tmp),1):        
-        if tmp[i]<lb[i]:
-            tmp[i]=lb[i]
-
-    #Apply upper bound
-    for i in range(0,len(tmp),1):
-        if tmp[i]>ub[i]:
-            tmp[i]=ub[i]
-  
-    return tmp
-
-#---------------------------------------------------------------------------------------# 
-def Rejection_Bounds(parent,child,stepsize,lb,ub,S):
-    """
-    Application of problem boundaries to generated solutions. Adjusts step size for all rejected
-    solutions until within the boundaries.  
-   
-    Parameters
-    ==========
-    parent : array
-        The current system designs
-    child : array
-        The proposed new system designs
-    stepsize : float
-        The Levy flight stepsize
-    lb : array
-        The lower bounds of the design variable(s)
-    ub : array
-        The upper bounds of the design variable(s)
-    S : Gnowee Settings Object    
-        An object representing the settings for the Gnowee optimization algorithm
-   
-    Returns
-    =======
-    child : array
-        The new system design that are within problem boundaries
-    """
-    
-    assert len(child)==len(lb), 'Child and lb best have different # of design variables in Rejection_Bounds function.'
-    assert len(ub)==len(lb), 'Boundaries best have different # of design variables in Rejection_Bounds function.'
-            
-    for i in range(0,len(child),1): 
-        change_count=0
-        while child[i]<lb[i] or child[i]>ub[i]:
-            if change_count >= 5:
-                child[i]=cp.copy(parent[i])
-            else:
-                stepsize[i]=stepsize[i]/2.0  
-                child[i]=child[i]-stepsize[i]
-                change_count+=1
-    return child
-
-#---------------------------------------------------------------------------------------#
-class WeightedRandomGenerator(object):
-    """
-    Defines a class of weights to be used to select number of instances in array randomly with
-    linear weighting.  
-   
-    Parameters
-    ==========
-    self : object
-        Current instance of the class
-    weights : array
-        The array of weights (Higher = more likely to be selected)
-   
-    Returns
-    =======
-    bisect.bisect_right(self.totals, rnd) : integer
-        The randomly selected index of the weights array
-    """
-    
-    def __init__(self, weights):
-        self.totals = []
-        running_total = 0
-
-        for w in weights:
-            running_total += w
-            self.totals.append(running_total)
-
-    def next(self):
-        rnd = np.random.rand() * self.totals[-1]
-        return bisect.bisect_right(self.totals, rnd)
-
-    def __call__(self):
-        return self.next()
